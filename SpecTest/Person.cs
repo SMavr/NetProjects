73da52fc7ccb48cf2a4f5bc631a99ed3fa08e208
Stamatis Mavrikis <stam.mavrikis@gmail.com>
2020-10-24 18:34:04 +0300

    Added some stuff

---
+++ SpecTest/Person.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SpecTest
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public int Experierience { get; set; }
+    }
+}
