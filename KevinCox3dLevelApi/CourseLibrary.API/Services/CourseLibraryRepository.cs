166c79981ca505a3cd07199979d1de93095d3f61
Stamatis Mavrikis <smavrikis@benefit.sf>
2020-11-22 19:11:30 +0200

    Applied sorting

--- KevinCox3dLevelApi/CourseLibrary.API/Services/CourseLibraryRepository.cs
+++ KevinCox3dLevelApi/CourseLibrary.API/Services/CourseLibraryRepository.cs
@@ -1,6 +1,7 @@
 ﻿using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Entities;
 using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
 using CourseLibrary.API.ResourceParameters;
 using System;
 using System.Collections.Generic;
@@ -11,10 +12,13 @@ namespace CourseLibrary.API.Services
     public class CourseLibraryRepository : ICourseLibraryRepository, IDisposable
     {
         private readonly CourseLibraryContext _context;
+        private readonly IPropertyMappingService _propertyMappingService;
 
-        public CourseLibraryRepository(CourseLibraryContext context )
+        public CourseLibraryRepository(CourseLibraryContext context,
+            IPropertyMappingService propertyMappingService)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
+            this._propertyMappingService = propertyMappingService;
         }
 
         public void AddCourse(Guid authorId, Course course)
@@ -150,11 +154,14 @@ namespace CourseLibrary.API.Services
 
             if(!string.IsNullOrWhiteSpace(authorsResourceParameters.OrderBy))  
             {
-                if(authorsResourceParameters.OrderBy.ToLowerInvariant() == "name")
-                {
-                    collection = collection.OrderBy(a => a.FirstName).ThenBy(a => a.LastName);
-                }
+                // get property mapping dictionary
+                var authorPropertyMappingDictionary =
+                    _propertyMappingService.GetPropertyMapping<AuthorDto, Author>();
+
+                collection = collection.ApplySort(authorsResourceParameters.OrderBy, 
+                    authorPropertyMappingDictionary);
             }
+
 
             return PagedList<Author>.Create(collection,
                 authorsResourceParameters.PageNumber,
