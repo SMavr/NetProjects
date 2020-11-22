6bad22bfb2bba8907bb3cd04afa13ed245ef1c68
Stamatis Mavrikis <smavrikis@benefit.sf>
2020-11-22 18:05:38 +0200

    Added sortings

---
+++ KevinCox3dLevelApi/CourseLibrary.API/Services/PropetyMapping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Services
+{
+    public class PropetyMapping<TSource, TDestination> : IPropertyMapping
+    {
+        public Dictionary<string, PropertyMappingValue> _mappingDictionary { get; private set; }
+
+        public PropetyMapping(Dictionary<string, PropertyMappingValue> mappingDictionary)
+        {
+            _mappingDictionary = mappingDictionary ??
+                throw new ArgumentNullException(nameof(mappingDictionary));
+        }
+    }
+}
