d082719b92ac56ebb1b607829dd7ff635fde2633
Stamatis Mavrikis <smavrikis@benefit.sf>
2020-11-22 16:23:08 +0200

    Added new properties

--- KevinCox3dLevelApi/CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
+++ KevinCox3dLevelApi/CourseLibrary.API/ResourceParameters/AuthorsResourceParameters.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CourseLibrary.API.ResourceParameters
+﻿namespace CourseLibrary.API.ResourceParameters
 {
     public class AuthorsResourceParameters
     {
         public string MainCategory { get; set; }
         public string SearchQuery { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
     }
 }
