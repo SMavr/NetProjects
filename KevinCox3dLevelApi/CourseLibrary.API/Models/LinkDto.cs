abacbe9f5f080c097d58998f5a46c9f883483e98
smavrikis <smavrikis@BNFT-WEBPC6>
2020-11-26 16:52:05 +0200

    Added link dto

---
+++ KevinCox3dLevelApi/CourseLibrary.API/Models/LinkDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Models
+{
+    public class LinkDto
+    {
+        /// <summary>
+        /// Link url
+        /// </summary>
+        public string Href { get; private set; }
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        public string Rel { get; private set; }
+
+        /// <summary>
+        /// GET, POST etc.
+        /// </summary>
+        public string Method { get; private set; }
+
+
+        public LinkDto(string href, string rel, string method)
+        {
+            Href = href;
+            Rel = rel;
+            Method = method;
+        }
+    }
+}
