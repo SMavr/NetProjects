e7ade9755aa8729c677f679cd64c003240135ea8
smavrikis <smavrikis@BNFT-WEBPC6>
2020-12-06 18:24:19 +0200

    Added new project

--- KevinCox3dLevelApi/CourseLibrary.API/Controllers/CoursesController.cs
+++ KevinCox3dLevelApi/CourseLibrary.API/Controllers/CoursesController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
+using Marvin.Cache.Headers;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
@@ -46,6 +47,8 @@ namespace CourseLibrary.API.Controllers
 
         [HttpGet("{courseId}", Name = "GetCourseForAuthor")]
         //[ResponseCache(Duration = 120)] // This is applied specifically to this method.
+        [HttpCacheExpiration(CacheLocation = CacheLocation.Public, MaxAge = 1000)]
+        [HttpCacheValidation(MustRevalidate = false)]
         public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
         {
             if (!_courseLibraryRepository.AuthorExists(authorId))
