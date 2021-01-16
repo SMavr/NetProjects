82b49a47f6b63a0b40136933e2732f6e040d02a6
smavrikis <smavrikis@BNFT-WEBPC6>
2021-01-16 17:23:46 +0200

    Can get movies

--- HttpClientEx/Movies.Client/Services/CRUDService.cs
+++ HttpClientEx/Movies.Client/Services/CRUDService.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Movies.Client.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,9 +10,30 @@ namespace Movies.Client.Services
 {
     public class CRUDService : IIntegrationService
     {
+        private static HttpClient httpClient = new HttpClient();
+
+        public CRUDService()
+        {
+            // set up HttpClient instance
+            httpClient.BaseAddress = new Uri("http://localhost:57863");
+            httpClient.Timeout = new TimeSpan(0, 0, 30);
+        }
+
         public async Task Run()
         {
-        
+            await GetResource();
+        }
+
+
+        public async Task GetResource()
+        {
+            var respose = await httpClient.GetAsync("api/movies");
+            
+            respose.EnsureSuccessStatusCode();
+
+            var content = await respose.Content.ReadAsStringAsync();
+
+            var movies = JsonConvert.DeserializeObject<IEnumerable<Movie>>(content);
         }
     }
 }
