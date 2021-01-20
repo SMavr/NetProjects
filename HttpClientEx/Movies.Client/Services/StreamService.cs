56d5c6ffd2333bdcdd6bd3bba58309663839416d
smavrikis <smavrikis@BNFT-WEBPC6>
2021-01-20 15:44:36 +0200

    Started implementing stream

--- HttpClientEx/Movies.Client/Services/StreamService.cs
+++ HttpClientEx/Movies.Client/Services/StreamService.cs
@@ -1,14 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Movies.Client.Services
 {
     public class StreamService : IIntegrationService
-    { 
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        public StreamService()
+        {
+            // set up HttpClient instance
+            httpClient.BaseAddress = new Uri("http://localhost:57863");
+            httpClient.Timeout = new TimeSpan(0, 0, 30);
+            httpClient.DefaultRequestHeaders.Clear();
+        }
+
         public async Task Run()
-        { 
+        {
         }          
+
+        //"d8663e5e-7494-4f81-8739-6e0de1bea7ee"
+
+        private async Task GetPsosterWithStream()
+        {
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/posters/{Guid.NewGuid()}");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await httpClient.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+
+            var stream = await response.Content.ReadAsStringAsync();
+        }
     }
 }
