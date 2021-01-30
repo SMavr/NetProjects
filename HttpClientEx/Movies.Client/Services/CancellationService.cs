930ea7537aa8c2986b2bb4c341261dea12f81b22
smavrikis <smavrikis@BNFT-WEBPC6>
2021-01-30 18:09:31 +0200

    Started with cancellation

--- HttpClientEx/Movies.Client/Services/CancellationService.cs
+++ HttpClientEx/Movies.Client/Services/CancellationService.cs
@@ -1,14 +1,55 @@
-﻿using System;
+﻿using Movies.Client.Models;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Movies.Client.Services
 {
     public class CancellationService : IIntegrationService
     {
+
+        private static HttpClient httpClient = new HttpClient(new HttpClientHandler()
+        {
+            AutomaticDecompression = System.Net.DecompressionMethods.GZip
+        });
+
+        public CancellationService()
+        {
+            // set up HttpClient instance
+            httpClient.BaseAddress = new Uri("http://localhost:57863");
+            httpClient.Timeout = new TimeSpan(0, 0, 30);
+            httpClient.DefaultRequestHeaders.Clear();
+        }
+
         public async Task Run()
         {
+            await GetTrailerAndCancel();
+        }
+
+        private async Task GetTrailerAndCancel()
+        {
+            var request = new HttpRequestMessage(
+             HttpMethod.Get,
+             $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/trailers/{Guid.NewGuid()}");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.CancelAfter(2);
+
+            using (var response = await httpClient.SendAsync(request,
+                HttpCompletionOption.ResponseHeadersRead, cancellationTokenSource.Token))
+            {
+                var stream = await response.Content.ReadAsStreamAsync();
+
+                response.EnsureSuccessStatusCode();
+                var trailer = stream.ReadAndDeserializeFromJson<Trailer>();
+            }
+
         }
     }
 }
