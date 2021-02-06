aad838af5feaeed920cf6ade374cae88eae74995
smavrikis <smavrikis@BNFT-WEBPC6>
2021-02-06 17:14:18 +0200

    Parse validation error

--- HttpClientEx/Movies.Client/Services/DealingWithErrorsAndFaultsService.cs
+++ HttpClientEx/Movies.Client/Services/DealingWithErrorsAndFaultsService.cs
@@ -1,4 +1,5 @@
 ﻿using Movies.Client.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -24,7 +25,8 @@ namespace Movies.Client.Services
 
         public async Task Run()
         {
-            await GetMovieAndDealWithInvalidRespones(cancellationTokenSource.Token);
+            //await GetMovieAndDealWithInvalidRespones(cancellationTokenSource.Token);
+            await PostMovieAndHadleValidationErrors(cancellationTokenSource.Token);
         }
 
         private async Task GetMovieAndDealWithInvalidRespones(
@@ -63,6 +65,59 @@ namespace Movies.Client.Services
                 response.EnsureSuccessStatusCode();
 
                 var movies = stream.ReadAndDeserializeFromJson<List<Movie>>();
+            }
+        }
+
+        private async Task PostMovieAndHadleValidationErrors(
+            CancellationToken cancellationToken)
+        {
+            var httpClient = httpClientFactory.CreateClient("MoviesClient");
+
+            var movieToCreate = new MovieForCreation()
+            {
+                Title = "Reservoir Dogs",
+                Description = "Test",
+                DirectorId = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+                ReleaseDate = new DateTimeOffset(new DateTime(1992, 9, 2)),
+                Genre = "Crime, Drama"
+            };
+
+            var serializedMovieToCreate = JsonConvert.SerializeObject(movieToCreate);
+
+            using (var request = new HttpRequestMessage(
+               HttpMethod.Post,
+              "api/movies"))
+            {
+
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+                request.Content = new StringContent(serializedMovieToCreate);
+                request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+
+                using (var response = await httpClient.SendAsync(request,
+                            HttpCompletionOption.ResponseHeadersRead,
+                            cancellationToken))
+                {
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        if(response.StatusCode == System.Net.HttpStatusCode.UnprocessableEntity)
+                        {
+                            var errorStream = await response.Content.ReadAsStreamAsync();
+                            var validationErrors = errorStream.ReadAndDeserializeFromJson<object>();
+                            Console.WriteLine(validationErrors);
+                            return;
+                        }
+                        else
+                        {
+                            response.EnsureSuccessStatusCode();
+                        }
+                    }
+
+                    var stream = await response.Content.ReadAsStreamAsync();
+                    var movie = stream.ReadAndDeserializeFromJson<Movie>();
+                }
             }
         }
     }
