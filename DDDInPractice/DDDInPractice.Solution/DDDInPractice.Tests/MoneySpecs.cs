376eddb093460a367c62dabba99ce1f5cf0a3ead
smavrikis <smavrikis@BNFT-WEBPC6>
2021-02-13 12:18:20 +0200

    Added two more tests

--- DDDInPractice/DDDInPractice.Solution/DDDInPractice.Tests/MoneySpecs.cs
+++ DDDInPractice/DDDInPractice.Solution/DDDInPractice.Tests/MoneySpecs.cs
@@ -14,17 +14,40 @@ namespace DDDInPractice.Tests
         [Fact]
         public void Sum_of_two_moneys_produces_correct_result()
         {
+            // Arrange
             Money money1 = new Money(1, 2, 3, 4, 5, 6);
             Money money2 = new Money(1, 2, 3, 4, 5, 6);
 
+            // Act
             Money sum = money1 + money2;
 
+            // Assert
             sum.OneCentCount.Should().Be(2);
             sum.TenCentCount.Should().Be(4);
             sum.QuarterCount.Should().Be(6);
             sum.OneDollarCount.Should().Be(8);
             sum.FiveDollarCount.Should().Be(10);
             sum.TwentyDollarCount.Should().Be(12);
+        }
+
+        [Fact]
+        public void Two_money_instances_equal_if_contain_the_same_money_amount()
+        {
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            money1.Should().Be(money2);
+            money1.GetHashCode().Should().Be(money2.GetHashCode());
+        }
+
+        [Fact]
+        public void Two_money_instances_do_not_equal_if_contain_different_money_amount()
+        {
+            Money dollar = new Money(0, 0, 0, 1, 0, 0);
+            Money hundredCents = new Money(100, 0, 0, 0, 0, 0);
+
+            dollar.Should().NotBe(hundredCents);
+            dollar.GetHashCode().Should().NotBe(hundredCents.GetHashCode());
         }
     }
 }
