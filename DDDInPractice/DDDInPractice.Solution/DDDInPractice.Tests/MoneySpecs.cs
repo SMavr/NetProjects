874824467952cd23b83f091530b6af3f48504127
smavrikis <smavrikis@BNFT-WEBPC6>
2021-02-13 12:11:57 +0200

    Test project

---
+++ DDDInPractice/DDDInPractice.Solution/DDDInPractice.Tests/MoneySpecs.cs
@@ -0,0 +1,30 @@
+﻿using DDDInPractice.Logic;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DDDInPractice.Tests
+{
+    public class MoneySpecs
+    {
+        [Fact]
+        public void Sum_of_two_moneys_produces_correct_result()
+        {
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            Money sum = money1 + money2;
+
+            sum.OneCentCount.Should().Be(2);
+            sum.TenCentCount.Should().Be(4);
+            sum.QuarterCount.Should().Be(6);
+            sum.OneDollarCount.Should().Be(8);
+            sum.FiveDollarCount.Should().Be(10);
+            sum.TwentyDollarCount.Should().Be(12);
+        }
+    }
+}
