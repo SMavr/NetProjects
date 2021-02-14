cde69f82f4dd72537726feea1dd7e6b154add1fa
smavrikis <smavrikis@BNFT-WEBPC6>
2021-02-14 10:56:50 +0200

    Added snack and slot

---
+++ DDDInPractice/DDDInPractice.Solution/DDDInPractice.Logic/Slot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDInPractice.Logic
+{
+    public class Slot : Entity
+    {
+        public virtual Snack Snack { get; protected set; }
+        public virtual int Quantity { get; protected set; }
+        public virtual decimal Price { get; protected set; }
+        public virtual SnackMachine SnackMachine { get; protected set; }
+        public virtual int Position { get; protected set; }
+
+        protected Slot()
+        {
+        }
+
+        public Slot(SnackMachine snackMachine, int position, Snack snack, int quantity, decimal price)
+            : this()
+        {
+            SnackMachine = snackMachine;
+            Position = position;
+            Snack = snack;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
