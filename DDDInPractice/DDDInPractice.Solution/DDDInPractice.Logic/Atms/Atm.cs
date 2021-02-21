cb9611b2fa043b3e51a70268cb3b26d08ed12d98
smavrikis <smavrikis@BNFT-WEBPC6>
2021-02-21 19:17:39 +0200

    CanTakeMoney

--- DDDInPractice/DDDInPractice.Solution/DDDInPractice.Logic/Atms/Atm.cs
+++ DDDInPractice/DDDInPractice.Solution/DDDInPractice.Logic/Atms/Atm.cs
@@ -20,8 +20,25 @@ namespace DDDInPractice.Logic.Atms
             MoneyInside += money;
         }
 
+        public virtual string CanTakeMoney(decimal amount)
+        {
+            if (amount < 0m)
+                return "Invalid amount";
+
+            if (MoneyInside.Amount < amount)
+                return "Not enough money";
+
+            if (!MoneyInside.CanAllocate(amount))
+                return "Nont enough change";
+
+            return string.Empty;
+        }
+
         public virtual void TakeMoney(decimal amount)
         {
+            if (CanTakeMoney(amount) != string.Empty)
+                throw new InvalidOperationException();
+
             Money output = MoneyInside.Allocate(amount);
             MoneyInside -= output;
 
