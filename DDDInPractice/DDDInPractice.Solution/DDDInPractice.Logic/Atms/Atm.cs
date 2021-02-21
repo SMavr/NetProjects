4d4300793afa0c2b8a0d4c87f6add1d3443dcf4f
smavrikis <smavrikis@BNFT-WEBPC6>
2021-02-21 19:06:58 +0200

    Take_money_exchanges_money_with_commission

---
+++ DDDInPractice/DDDInPractice.Solution/DDDInPractice.Logic/Atms/Atm.cs
@@ -0,0 +1,30 @@
+﻿using DDDInPractice.Logic.Common;
+using DDDInPractice.Logic.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDInPractice.Logic.Atms
+{
+    public class Atm : AggregateRoot
+    {
+        public virtual Money MoneyInside { get; protected set; } = Money.None;
+        public virtual decimal MoneyCharged { get; protected set; }
+
+        public void LoadMoney(Money money)
+        {
+            MoneyInside += money;
+        }
+
+        public virtual void TakeMoney(decimal amount)
+        {
+            Money output = MoneyInside.Allocate(amount);
+            MoneyInside -= output;
+
+            decimal amountWithCommission = amount + amount * 0.01m;
+            MoneyCharged += amountWithCommission;
+        }
+    }
+}
---
+++ DDDInPractice/DDDInPractice.Solution/DDDInPractice.Tests/AtmSpecs.cs
@@ -0,0 +1,27 @@
+﻿using DDDInPractice.Logic.Atms;
+using DDDInPractice.Logic.SharedKernel;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DDDInPractice.Tests
+{
+    public class AtmSpecs
+    {
+        [Fact]
+        public void Take_money_exchanges_money_with_commission()
+        {
+            var atm = new Atm();
+            atm.LoadMoney(Money.Dollar);
+
+            atm.TakeMoney(1m);
+
+            atm.MoneyInside.Amount.Should().Be(0m);
+            atm.MoneyCharged.Should().Be(1.01m);
+        }
+    }
+}
