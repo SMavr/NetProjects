05c449081ca87db7c490858a248477c1a9d6d2f0
smavrikis <smavrikis@BNFT-WEBPC6>
2021-02-23 13:45:20 +0200

    Implemented handler

---
+++ DDDInPractice/DDDInPractice.Solution/DDDInPractice.Logic/Common/BalanceChangedEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace DDDInPractice.Logic.Common
+{
+    public class BalanceChangedEvent : IDomainEvent
+    {
+        public decimal Delta { get; set; }
+
+        public BalanceChangedEvent(decimal delta)
+        {
+            Delta = delta;
+        }
+    }
+}
---
+++ DDDInPractice/DDDInPractice.Solution/DDDInPractice.Logic/Management/BalanceChangedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using DDDInPractice.Logic.Common;
+
+namespace DDDInPractice.Logic.Management
+{
+    public class BalanceChangedEventHandler : IHandler<BalanceChangedEvent>
+    {
+        public void Handle(BalanceChangedEvent domainEvent)
+        {
+            var repository = new HeadOfficeRepository();
+            HeadOffice headOffice = HeadOfficeInstance.Instance;
+            headOffice.ChangeBalance(domainEvent.Delta);
+            repository.Save(headOffice);
+        }
+    }
+}
